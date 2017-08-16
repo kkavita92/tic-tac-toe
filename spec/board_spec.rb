68fdd6093ff57b4994c8dcf144442ad6d4e4f5a7
kkavita92 <kkavita92@gmail.com>
2017-08-16 09:48:28 +0800

    Initialises board with three sub-arrays with length of three each

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,8 +3,14 @@ require 'board'
 describe Board do
   subject(:board) { described_class.new }
 
-  it 'creates array of three arrays representing rows' do
+  it 'creates grid - array of three arrays representing rows' do
     expect(board.grid.length).to eq 3
+  end
+
+  it 'creates grid - each sub-array has three columns' do
+    expect(board.grid[0].length).to eq 3
+    expect(board.grid[1].length).to eq 3
+    expect(board.grid[2].length).to eq 3
   end
 
 end
