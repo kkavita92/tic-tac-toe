d22cf815501f819044f3b937622f35641a57a05b
kkavita92 <kkavita92@gmail.com>
2017-08-16 22:00:33 +0800

    Add all possible winning options to winner configuration

--- lib/board.rb
+++ lib/board.rb
@@ -44,7 +44,14 @@ class Board
 
   def winning_positions
     [
-      [@grid[0][0], @grid[0][1], @grid[0][2]]
+      [@grid[0][0], @grid[0][1], @grid[0][2]],
+      [@grid[1][0], @grid[1][1], @grid[1][2]],
+      [@grid[2][0], @grid[2][1], @grid[2][2]],
+      [@grid[0][0], @grid[1][0], @grid[2][0]],
+      [@grid[0][1], @grid[1][1], @grid[2][1]],
+      [@grid[0][2], @grid[1][2], @grid[2][2]],
+      [@grid[0][0], @grid[1][1], @grid[2][2]],
+      [@grid[0][2], @grid[1][1], @grid[2][0]], 
     ]
   end
 
--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -1,9 +1,9 @@
 require './lib/board'
 
 board = Board.new
-board.play("1")
-board.play("2")
 board.play("3")
+board.play("5")
+board.play("7")
 
 p board.grid
 p board.is_win?('X')
